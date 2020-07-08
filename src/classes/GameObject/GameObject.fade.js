40eeb5ac196680b771939a769edc5b2effad5341
Grimka <julien.decroix64@gmail.com>
2020-07-08 16:46:11 +0200

    Fix visibility when alpha == 0 || 1

--- src/classes/GameObject/GameObject.fade.js
+++ src/classes/GameObject/GameObject.fade.js
@@ -1,5 +1,5 @@
-import GameObject from 'DE.GameObject';
-import Time from 'DE.Time';
+import GameObject from "DE.GameObject";
+import Time from "DE.Time";
 
 /**
  * create a fade from alpha to alpha, with given duration time
@@ -10,30 +10,25 @@ import Time from 'DE.Time';
  * @param {Int} [duration=500] fade duration in ms
  * @example myObject.fade( 0.5, 1, 850 );
  */
-GameObject.prototype.fade = function( from, to, duration, force, callback )
-{
-  if ( force ) {
+GameObject.prototype.fade = function (from, to, duration, force, callback) {
+  if (force) {
     this.enable = true;
   }
-  
+
   var data = {
-    from      : from != undefined ? from : 1
-    ,to       : to != undefined ? to : 0
-    ,duration : duration || 500
-    ,oDuration: duration || 500
-    ,fadeScale: Math.abs( from - to )
-    ,done     : false
-    ,callback : callback
+    from: from != undefined ? from : 1,
+    to: to != undefined ? to : 0,
+    duration: duration || 500,
+    oDuration: duration || 500,
+    fadeScale: Math.abs(from - to),
+    done: false,
+    callback: callback,
   };
-  
-  data.dir       = data.from > to ? -1 : 1;
-  this.alpha     = from;
+
+  data.dir = data.from > to ? -1 : 1;
+  this.alpha = from;
   this._fadeData = data;
-  
-  if ( !this.visible && to > 0 ) {
-    this.visible = true;
-  }
-  
+
   return this;
 };
 
@@ -45,10 +40,9 @@ GameObject.prototype.fade = function( from, to, duration, force, callback )
  * @param {Int} [duration=500] fade duration in ms
  * @example myObject.fadeTo( 0.5, 850 ); // don't care if alpha is 0.2 or 0.8
  */
-GameObject.prototype.fadeTo = function( to, duration, force, callback )
-{
-  this.fade( this.alpha, to, duration, force, callback );
-  
+GameObject.prototype.fadeTo = function (to, duration, force, callback) {
+  this.fade(this.alpha, to, duration, force, callback);
+
   return this;
 };
 
@@ -62,14 +56,13 @@ GameObject.prototype.fadeTo = function( to, duration, force, callback )
  * @example // alpha = 0 in 850ms
  * myObject.fadeOut( 850 );
  */
-GameObject.prototype.fadeOut = function( duration, force, callback )
-{
-  if ( force ) {
+GameObject.prototype.fadeOut = function (duration, force, callback) {
+  if (force) {
     this.enable = true;
     this.alpha = this.alpha > 0 ? this.alpha : 1; // make sure to prevent any blink side effect
   }
-  
-  this.fade( this.alpha, 0, duration, force, callback );
+
+  this.fade(this.alpha, 0, duration, force, callback);
   return this;
 };
 
@@ -83,14 +76,13 @@ GameObject.prototype.fadeOut = function( duration, force, callback )
  * @example // alpha = 1 in 850ms
  * myObject.fadeIn( 850 );
  */
-GameObject.prototype.fadeIn = function( duration, force, callback )
-{
-  if ( force ) {
+GameObject.prototype.fadeIn = function (duration, force, callback) {
+  if (force) {
     this.enable = true;
     this.alpha = this.alpha < 1 ? this.alpha : 0; // make sure to prevent any blink side effect
   }
-  
-  this.fade( this.alpha, 1, duration, force, callback );
+
+  this.fade(this.alpha, 1, duration, force, callback);
   return this;
 };
 
@@ -99,31 +91,31 @@ GameObject.prototype.fadeIn = function( duration, force, callback )
  * @protected
  * @memberOf GameObject
  */
-GameObject.prototype.applyFade = function()
-{
-  if ( !this._fadeData.done ) {
-    this._fadeData.stepVal = Time.frameDelayScaled / this._fadeData.oDuration
-                            * this._fadeData.dir * this._fadeData.fadeScale;
+GameObject.prototype.applyFade = function () {
+  if (!this._fadeData.done) {
+    this._fadeData.stepVal =
+      (Time.frameDelayScaled / this._fadeData.oDuration) *
+      this._fadeData.dir *
+      this._fadeData.fadeScale;
     this.alpha += this._fadeData.stepVal;
     this._fadeData.duration -= Time.frameDelayScaled;
-    
-    if ( ( this._fadeData.dir < 0 && this.alpha <= this._fadeData.to )
-        || ( this._fadeData.dir > 0 && this.alpha >= this._fadeData.to )
-        || this.alpha < 0 || this.alpha > 1 ) {
+
+    if (
+      (this._fadeData.dir < 0 && this.alpha <= this._fadeData.to) ||
+      (this._fadeData.dir > 0 && this.alpha >= this._fadeData.to) ||
+      this.alpha < 0 ||
+      this.alpha > 1
+    ) {
       this.alpha = this._fadeData.to;
     }
-    
-    if ( this._fadeData.duration <= 0 ) {
+
+    if (this._fadeData.duration <= 0) {
       this._fadeData.done = true;
-      
-      if ( this.alpha == 0 ) {
-        this.visible = false;
-      }
-      
-      this.trigger( "fadeEnd", this );
-      
-      if ( this._fadeData.callback ) {
-        this._fadeData.callback.call( this );
+
+      this.trigger("fadeEnd", this);
+
+      if (this._fadeData.callback) {
+        this._fadeData.callback.call(this);
       }
     }
   }
