033ab0dcee3b208d3ab8e5ccdeeaf78df67ad43e
Quentin Mallen <mallen.quentin@gmail.com>
2023-06-26 15:47:38 +0200

    Interface plus vraie

--- src/classes/renderer/RendererInterface.d.ts
+++ src/classes/renderer/RendererInterface.d.ts
@@ -3,4 +3,14 @@ export default interface RendererInterface {
     preventCenter?: boolean;
     anchor?: PIXI.ObservablePoint | Point2D;
     tint: number;
+    setTint(value: number): void;
+    setHue(rotation: number, multiply: boolean): void;
+    setBlackAndWhite(multiply: boolean): void;
+    setSaturation(amount: number, multiply: boolean): void;
+    setBrightness(b: number, multiply: boolean): void;
+    setContrast(amount: number, multiply: boolean): void;
+    setGreyscale(scale: number, multiply: boolean): void;
+    setSize(width: number, height: number, preventCenter: boolean): void;
+    setScale(x: number | { x: number; y: number }, y?: number): void;
+    center(): void;
 }
--- src/classes/renderer/RendererInterface.ts
+++ src/classes/renderer/RendererInterface.ts
@@ -1,7 +1,17 @@
 import * as PIXI from 'pixi.js';
 
-export default interface RendererInterface{
+export default interface RendererInterface {
     preventCenter?: boolean;
     anchor?: PIXI.ObservablePoint | Point2D;
     tint: number;
+    setTint(value: number): void;
+    setHue(rotation: number, multiply: boolean): void;
+    setBlackAndWhite(multiply: boolean): void;
+    setSaturation(amount: number, multiply: boolean): void;
+    setBrightness(b: number, multiply: boolean): void;
+    setContrast(amount: number, multiply: boolean): void;
+    setGreyscale(scale: number, multiply: boolean): void;
+    setSize(width: number, height: number, preventCenter: boolean): void;
+    setScale(x: number | { x: number; y: number }, y?: number): void;
+    center(): void;
 }
